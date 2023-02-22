35384af64252105c85ac963b485158c485964814
Ayelen Montenegro <98491462+ayee22@users.noreply.github.com>
2023-02-22 02:34:27 -0300

    Update index.js

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -17,16 +17,16 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
+require("dotenv").config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { getTemperaments } = require('../api/src/controllers/index')
-const port = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
-  server.listen(port, () => {
+  server.listen(PORT, () => {
     getTemperaments()
-    console.log('%s listening at ', port); // eslint-disable-line no-console
+    console.log('%s listening at ', PORT); // eslint-disable-line no-console
   });
 });
