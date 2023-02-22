23765c2abf388962e8c230e6b78fa0f4d6ebece1
ayee22 <ayemontenegro220@gmail.com>
2023-02-22 01:52:20 -0300

    railway_deploy_db

--- PI-Dogs-main/api/src/db.js
+++ PI-Dogs-main/api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  RAILWAY_DEPLOY_DB
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize( RAILWAY_DEPLOY_DB, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
